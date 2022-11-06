ca240c95177bedb657839104bd6f6e50793d1041
vladdd183 <vladdd183@gmail.com>
2022-11-06 12:57:18 +0000

    class init

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 import os
 import sys
 from pydub import AudioSegment
+
+
+# mixer class to cut and mix mp3 files
+class Mixer:
+    # init class with path to mp3 files
+    def __init__(self, path):
+        self.path = path
+        self.files = []
+        for file in os.listdir(self.path):
+            if file.endswith(".mp3"):
+                self.files.append(file)
+
+        self.cuted_files = {}
