d85b523fb4c9c38cfc459d4beb44d006cf78a9e5
vladdd183 <vladdd183@gmail.com>
2022-11-06 12:57:55 +0000

    some methods

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ class Mixer:
                 self.files.append(file)
 
         self.cuted_files = {}
+
+    # cut mp3 file
+    def cut(self, file, start, end):
+        song = AudioSegment.from_mp3(self.path + file)
+        song = song[start * 1000:end * 1000]
+        self.cuted_files[file] = song
+
+    # print list of files
+    def print_list(self):
+        for idx, file in enumerate(self.files):
+            print('===[' + str(idx) + ']', file)
